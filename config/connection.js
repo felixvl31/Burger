a8d395beda60f0c1f26ecea14963b49a9ddf775f
Felix Villalobos <felix.evl@gmail.com>
2019-03-23 21:18:30 -0500

    Handle Disconnection

--- config/connection.js
+++ config/connection.js
@@ -11,21 +11,54 @@ var mysql = require("mysql");
 // });
 
 //Heroku Connection
-var connection = mysql.createConnection({
+// var connection = mysql.createConnection({
+//   host: "us-cdbr-iron-east-03.cleardb.net",
+//   user: "ba3bc920b5c303",
+//   password: "04d51ce8",
+//   database: "heroku_6a34609d9f1730b"
+// });
+
+// // Make connection.
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
+
+var db_config = {
   host: "us-cdbr-iron-east-03.cleardb.net",
   user: "ba3bc920b5c303",
   password: "04d51ce8",
   database: "heroku_6a34609d9f1730b"
-});
+};
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); 
+
+  connection.connect(function(err) {            
+    if(err) {                                     
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); 
+    }
+                                         
+  });                                     
+
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+      handleDisconnect();                         
+    } else {                                      
+      throw err;                                  
+    }
+  });
+}
+
+handleDisconnect();
 
 // Export connection for our ORM to use.
 module.exports = connection;
