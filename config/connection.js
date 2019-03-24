4148ee71e48dad6eb4e2c48407397c312bc0b9ca
Felix Villalobos <felix.evl@gmail.com>
2019-03-23 20:20:06 -0500

    Test Heroku DB

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,21 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+//Local Connection
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "",
+//   database: "burgers_db"
+// });
+
+//Heroku Connection
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
+  host: "us-cdbr-iron-east-03.cleardb.net",
+  user: "ba3bc920b5c303",
+  password: " 04d51ce8",
+  database: " heroku_6a34609d9f1730b"
 });
 
 // Make connection.
