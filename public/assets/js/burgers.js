d814fe269860bb7c92effb8ff60c1b5c20484bcc
Felix Villalobos <felix.evl@gmail.com>
2019-03-23 21:47:14 -0500

    Validation to not add empty burgers

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -24,6 +24,7 @@ $(function() {
   $(".create-form").on("submit", function(event) {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
+    if ($("#burg").val().trim() === "") return;
 
     var newBurger = {
       name: $("#burg").val().trim(),
